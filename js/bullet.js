4e6924e4259be55ef2dfd06a58c9714b7143a8c0
Jogum Cannegieter <39035700+JadenDragon@users.noreply.github.com>
2021-04-15 23:34:44 +0200

    fixed audio output to work more consistantly for feedback

--- js/bullet.js
+++ js/bullet.js
@@ -1,3 +1,6 @@
+let sound;
+//let explosion = new Audio("sound/zapsplat_explosions_short_small_explosion_no_tail_003_62730.mp3");
+
 class Bullet {
     constructor(x, y) {
         this.r = 5;
@@ -10,17 +13,23 @@ class Bullet {
     show() {
         if (this.visible === true) {
             fill(0, 180, 230);
-            rect(this.x, this.y, 5, 20);        
+            rect(this.x, this.y, 5, 20);
         } else if (this.visible === false) {
             rect(-10, -10, 0, 0);
         }
-        this.y-=this.velY;
+        this.y -= this.velY;
     }
 
     hits(block) {
         //console.log(block);
+        //explosion.play();
+        //explosion.volume = 0.1;
         return collideRectRect(this.x, this.y, this.r, this.r, block.x, block.y, block.r, block.r);
     }
 
-    
+    // feedback(){
+    //     sound = loadSound("sound/zapsplat_explosions_short_small_explosion_no_tail_003_62730.mp3", loaded);
+    // }
+
+
 }
--- js/sketch.js
+++ js/sketch.js
@@ -6,153 +6,150 @@ let bullets = [];
 let blocks = [];
 let counter = 0;
 let gameOver = false;
+let startSong;
 
 let video;
 let flippedVideo;
+
+let audio = new Audio("sound/thunder.mp3");
+let explosion = new Audio("sound/zapsplat_explosions_short_small_explosion_no_tail_003_62730.mp3");
 
 // Global variable to store the classifier
 let classifier;
 
 // Label
 let label = 'listening...';
-  
+
 // Teachable Machine model URL:
 let soundModel = 'https://teachablemachine.withgoogle.com/models/2CCC-uNZj/';
-  
-  
+
+
 function preload() {
-  // Load the model
-  classifier = ml5.soundClassifier(soundModel + 'model.json');
+    // Load the model
+    classifier = ml5.soundClassifier(soundModel + 'model.json');
+    startSong = loadSound("sound/science_fiction_machine_object_obliterate.mp3");
 }
 
+// function loaded() {
+//   sound.play();
+// }
+
 function setup() {
-  createCanvas(600, 600);
-  video = createCapture(VIDEO);
-  video.hide();
-  flippedVideo = ml5.flipImage(video);
+    createCanvas(600, 600);
+    video = createCapture(VIDEO);
+    video.hide();
+    //flippedVideo = ml5.flipImage(video);
+    startSong.play();
+    audio.volume = 0.12;
+    //audio.play();
 
-  poseNet = ml5.poseNet(video, {
-    //flipHorizontal:true
-  }, modelLoaded);
-  poseNet.on('pose', gotPoses);
+    poseNet = ml5.poseNet(video, {
+        //flipHorizontal:true
+    }, modelLoaded);
+    poseNet.on('pose', gotPoses);
 
-  // Start classifying
-  // The sound model will continuously listen to the microphone
-  classifier.classify(gotResult);
+    // Start classifying
+    // The sound model will continuously listen to the microphone
+    classifier.classify(gotResult);
 
-  player = new Player();
-  block = new Block();
-  pCoords = player.getCoords();
-  //console.log(pCoords);
-  //console.log(flippedVideo);
+    player = new Player();
+    block = new Block();
+    pCoords = player.getCoords();
+    //console.log(pCoords);
+    //console.log(flippedVideo);
 }
 
 function gotPoses(poses) {
-  if (poses.length > 0) {
-    pose = poses[0].pose;
-    skeleton = poses[0].skeleton;
-  }
+    if (poses.length > 0) {
+        pose = poses[0].pose;
+        skeleton = poses[0].skeleton;
+    }
 }
 
 function modelLoaded() {
-  console.log('poseNet ready');
+    console.log('poseNet ready');
 }
 
 function draw() {
-  background(220);
-  player.show();
-  // block.show();
-  // block.fall();
-  pCoords = player.getCoords();
-  player.move();
-  //player.shoot();
-  
-  
-  for (let index = 0; index < bullets.length; index++) {
-    let bullet = bullets[index];
-    bullet.show();
-    if (bullet.hits(block)) {
-      //remove(bullets, index);
-      bullet.visible = false;
-      console.log("DED bullet!");
-    }  
-  }
-  
-  if (random(1) < 0.005) {
-    blocks.push(new Block());
-  }
+    background(220);
+    player.show();
+    // block.show();
+    // block.fall();
+    pCoords = player.getCoords();
+    player.move();
+    //player.shoot();
 
-  blocks.forEach(function(block, i) {
-    block.show();
-    block.fall();
-    //console.log(block.y);
-    collCheck = player.hits(block);
-    if (collCheck === true) {
-      console.log("PlayerCollisionDetected");
-      player.setCoords(pCoords[0], pCoords[1]);
-      noLoop();
-      gameOver === true;
-      //checkGame();
-    } 
 
-    // if (block.y >= height) {
-    //   counter +=1;
-    //   console.log(counter);
-    // }
-  })
+    for (let index = 0; index < bullets.length; index++) {
+        let bullet = bullets[index];
+        bullet.show();
+        if (bullet.hits(block)) {
+            //remove(bullets, index);
+            bullet.visible = false;
+            explosion.play();
+            console.log("DED bullet!");
+        }
+    }
 
-  // for (let block of blocks){
-  //   block.show();
-  //   block.fall();
-  //   //console.log(block.y);
-  //   collCheck = player.hits(block);
-  //   if (collCheck === true) {
-  //     console.log("PlayerCollisionDetected");
-  //     player.setCoords(pCoords[0], pCoords[1]);
-  //     //noLoop();
-  //   }
-  // }
+    if (random(1) < 0.005) {
+        blocks.push(new Block());
+    }
 
-  // Draw the label in the canvas
-  fill(255);
-  textSize(32);
-  textAlign(CENTER, CENTER);
-  text(label, width / 2, height / 2);
-  
+    blocks.forEach(function(block, i) {
+        block.show();
+        block.fall();
+        //console.log(block.y);
+        collCheck = player.hits(block);
+        if (collCheck === true) {
+            console.log("PlayerCollisionDetected");
+            player.setCoords(pCoords[0], pCoords[1]);
+            noLoop();
+            gameOver === true;
+            //checkGame();
+        }
+
+    })
+
+    // Draw the label in the canvas
+    fill(255);
+    textSize(32);
+    textAlign(CENTER, CENTER);
+    text(label, width / 2, height / 2);
+
 }
 
 // The model recognizing a sound will trigger this event
 function gotResult(error, results) {
-  if (error) {
-    console.error(error);
-    return;
-  }
-  // The results are in an array ordered by confidence.
-  // console.log(results[0]);
-  label = results[0].label;
+    if (error) {
+        console.error(error);
+        return;
+    }
+    // The results are in an array ordered by confidence.
+    // console.log(results[0]);
+    label = results[0].label;
 
-  if (label === 'Shoot') {
-    player.shoot();
-  } else if (label === 'Clap') {
-    // window.location.reload();
-    // console.log("RESTART!");
-    checkGame();
-  }
+    if (label === 'Shoot') {
+        player.shoot();
+    } else if (label === 'Clap') {
+        // window.location.reload();
+        console.log("RESTART!");
+        checkGame();
+    }
 }
 
 
 function remove(array, index) {
-  array.splice(index, 1);
+    array.splice(index, 1);
 }
 
 //restarts game on CLAP
 function checkGame() {
-  if (gameOver == true) {
-    window.location.reload();
-    console.log("YOU DIED!");
-  }
+    if (gameOver == true) {
+        window.location.reload();
+        console.log("YOU DIED!");
+    }
 }
 
 // function onLoad() {
-  
+
 // }
